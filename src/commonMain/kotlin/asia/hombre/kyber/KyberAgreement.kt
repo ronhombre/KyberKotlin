ronhombre/KyberKotlin

commit 69ff060e7c2e3936e46666cc0d5543e6048ee1b3

Improve code by replacing companion objects inside of classes into objects themselves

src/commonMain/kotlin/asia/hombre/kyber/KyberKeyGenerator.kt
@@ -36,127 +36,122 @@ import kotlin.jvm.JvmSynthetic
  */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
-sealed class KyberKeyGenerator private constructor() {
-    companion object {
-        /**
-         * Generate ML-KEM keys.
-         *
-         * This method is the ML-KEM.KeyGen() specified in NIST FIPS 203.
-         *
-         * @param parameter [KyberParameter] of the keys to be generated.
-         * @return [KyberKEMKeyPair] - Contains the Encapsulation and Decapsulation Key.
-         */
-        @JvmStatic
-        fun generate(parameter: KyberParameter): KyberKEMKeyPair {
-            val secureRandom = SecureRandom()
-            return generate(
-                parameter,
-                secureRandom.nextBytesOf(KyberConstants.N_BYTES),
-                secureRandom.nextBytesOf(KyberConstants.N_BYTES)
-            )
-        }
+object KyberKeyGenerator {
+    /**
+     * Generate ML-KEM keys.
+     *
+     * This method is the ML-KEM.KeyGen() specified in NIST FIPS 203.
+     *
+     * @param parameter [KyberParameter] of the keys to be generated.
+     * @return [KyberKEMKeyPair] - Contains the Encapsulation and Decapsulation Key.
+     */
+    @JvmStatic
+    fun generate(parameter: KyberParameter): KyberKEMKeyPair {
+        val secureRandom = SecureRandom()
+        return generate(
+            parameter,
+            secureRandom.nextBytesOf(KyberConstants.N_BYTES),
+            secureRandom.nextBytesOf(KyberConstants.N_BYTES)
+        )
+    }
 
+    /**
+     * Internal Generate function for ML-KEM keys for testing purposes.
+     *
+     * This method is the ML-KEM.KeyGen() specified in NIST FIPS 203.
+     *
+     * @param parameter [KyberParameter] of the keys to be generated.
+     * @param randomSeed [ByteArray]
+     * @param pkeSeed [ByteArray]
+     * @return [KyberKEMKeyPair] - Contains the Encapsulation and Decapsulation Key.
+     */
+    @JvmSynthetic
+    internal fun generate(parameter: KyberParameter, randomSeed: ByteArray, pkeSeed: ByteArray): KyberKEMKeyPair {
+        val pkeKeyPair = PKEGenerator.generate(parameter, pkeSeed)
+
+        val hash = KeccakHash.generate(KeccakParameter.SHA3_256, pkeKeyPair.encryptionKey.fullBytes, 32)
+
+        return KyberKEMKeyPair(
+            KyberEncapsulationKey(pkeKeyPair.encryptionKey),
+            KyberDecapsulationKey(pkeKeyPair.decryptionKey, pkeKeyPair.encryptionKey, hash, randomSeed)
+        )
+    }
+
+    /**
+     * A generator class for K-PKE Keys.
+     *
+     * This subclass contains K-PKE.KeyGen() all according to NIST FIPS 203.
+     *
+     * @author Ron Lauren Hombre
+     */
+    internal object PKEGenerator {
         /**
-         * Internal Generate function for ML-KEM keys for testing purposes.
+         * Internal Generate function for K-PKE keys for testing purposes.
          *
-         * This method is the ML-KEM.KeyGen() specified in NIST FIPS 203.
+         * This method is the K-PKE.KeyGen() specified in NIST FIPS 203.
          *
          * @param parameter [KyberParameter] of the keys to be generated.
-         * @param randomSeed [ByteArray]
-         * @param pkeSeed [ByteArray]
-         * @return [KyberKEMKeyPair] - Contains the Encapsulation and Decapsulation Key.
+         * @param byteArray Random [ByteArray] which is a seed.
+         * @return [KyberPKEKeyPair] - Contains the Encryption and Decryption Key.
          */
         @JvmSynthetic
-        internal fun generate(parameter: KyberParameter, randomSeed: ByteArray, pkeSeed: ByteArray): KyberKEMKeyPair {
-            val pkeKeyPair = PKEGenerator.generate(parameter, pkeSeed)
+        fun generate(parameter: KyberParameter, byteArray: ByteArray): KyberPKEKeyPair {
+            val seeds = KeccakHash.generate(KeccakParameter.SHA3_512, byteArray)
 
-            val hash = KeccakHash.generate(KeccakParameter.SHA3_256, pkeKeyPair.encryptionKey.fullBytes, 32)
+            //Security Feature
+            byteArray.fill(0, 0, byteArray.size)
 
-            return KyberKEMKeyPair(
-                KyberEncapsulationKey(pkeKeyPair.encryptionKey),
-                KyberDecapsulationKey(pkeKeyPair.decryptionKey, pkeKeyPair.encryptionKey, hash, randomSeed)
-            )
-        }
+            val nttSeed = seeds.copyOfRange(0, 32)
+            val cbdSeed = seeds.copyOfRange(32, 64)
 
-        /**
-         * A generator class for K-PKE Keys.
-         *
-         * This subclass contains K-PKE.KeyGen() all according to NIST FIPS 203.
-         *
-         * @author Ron Lauren Hombre
-         */
-        internal class PKEGenerator {
-            companion object {
-                /**
-                 * Internal Generate function for K-PKE keys for testing purposes.
-                 *
-                 * This method is the K-PKE.KeyGen() specified in NIST FIPS 203.
-                 *
-                 * @param parameter [KyberParameter] of the keys to be generated.
-                 * @param byteArray Random [ByteArray] which is a seed.
-                 * @return [KyberPKEKeyPair] - Contains the Encryption and Decryption Key.
-                 */
-                @JvmSynthetic
-                fun generate(parameter: KyberParameter, byteArray: ByteArray): KyberPKEKeyPair {
-                    val seeds = KeccakHash.generate(KeccakParameter.SHA3_512, byteArray)
+            seeds.fill(0, 0, seeds.size) //Security Feature
 
-                    //Security Feature
-                    byteArray.fill(0, 0, byteArray.size)
+            val matrix = Array(parameter.K) { Array(parameter.K) { IntArray(KyberConstants.N) } }
+            val secretVector = Array(parameter.K) { IntArray(KyberConstants.N) }
+            val noiseVector = Array(parameter.K) { IntArray(KyberConstants.N) }
 
-                    val nttSeed = seeds.copyOfRange(0, 32)
-                    val cbdSeed = seeds.copyOfRange(32, 64)
+            for((nonce, i) in (0..<parameter.K).withIndex()) {
+                for(j in 0..<parameter.K)
+                    matrix[i][j] = KyberMath.sampleNTT(KyberMath.xof(nttSeed, i.toByte(), j.toByte()))
 
-                    seeds.fill(0, 0, seeds.size) //Security Feature
+                secretVector[i] = KyberMath.samplePolyCBD(
+                    parameter.ETA1,
+                    KyberMath.prf(parameter.ETA1, cbdSeed, nonce.toByte())
+                )
+                secretVector[i] = KyberMath.NTT(secretVector[i])
 
-                    val matrix = Array(parameter.K) { Array(parameter.K) { IntArray(KyberConstants.N) } }
-                    val secretVector = Array(parameter.K) { IntArray(KyberConstants.N) }
-                    val noiseVector = Array(parameter.K) { IntArray(KyberConstants.N) }
-
-                    for((nonce, i) in (0..<parameter.K).withIndex()) {
-                        for(j in 0..<parameter.K)
-                            matrix[i][j] = KyberMath.sampleNTT(KyberMath.xof(nttSeed, i.toByte(), j.toByte()))
-
-                        secretVector[i] = KyberMath.samplePolyCBD(
-                            parameter.ETA1,
-                            KyberMath.prf(parameter.ETA1, cbdSeed, nonce.toByte())
-                        )
-                        secretVector[i] = KyberMath.NTT(secretVector[i])
-
-                        noiseVector[i] = KyberMath.samplePolyCBD(
-                            parameter.ETA1,
-                            KyberMath.prf(parameter.ETA1, cbdSeed, (nonce + parameter.K).toByte())
-                        )
-                        noiseVector[i] = KyberMath.NTT(noiseVector[i])
-                    }
-
-                    cbdSeed.fill(0, 0, cbdSeed.size) //Security Feature
-
-                    //Transposed ? Old Kyber v3
-                    val systemVector = KyberMath.vectorAddition(
-                        KyberMath.nttMatrixToVectorDot(matrix, secretVector, true),
-                        noiseVector
-                    )
-
-                    val encryptionKeyBytes = ByteArray(parameter.ENCRYPTION_KEY_LENGTH - KyberConstants.N_BYTES) //Excluded nttSeed
-                    val decryptionKeyBytes = ByteArray(parameter.DECRYPTION_KEY_LENGTH)
-
-                    for(i in 0..<parameter.K) {
-                        //Security Features
-                        for(j in 0..<parameter.K) matrix[i][j].fill(0, 0, matrix[i][j].size)
-                        noiseVector[i].fill(0, 0, noiseVector[i].size)
-
-                        KyberMath.byteEncode(KyberMath.montVectorToVector(systemVector[i]), 12)
-                            .copyInto(encryptionKeyBytes, i * KyberConstants.ENCODE_SIZE)
-                        KyberMath.byteEncode(KyberMath.montVectorToVector(secretVector[i]), 12)
-                            .copyInto(decryptionKeyBytes, i * KyberConstants.ENCODE_SIZE)
-                    }
-
-                    return KyberPKEKeyPair(
-                        KyberEncryptionKey(parameter, encryptionKeyBytes, nttSeed),
-                        KyberDecryptionKey(parameter, decryptionKeyBytes)
-                    )
-                }
+                noiseVector[i] = KyberMath.samplePolyCBD(
+                    parameter.ETA1,
+                    KyberMath.prf(parameter.ETA1, cbdSeed, (nonce + parameter.K).toByte())
+                )
+                noiseVector[i] = KyberMath.NTT(noiseVector[i])
             }
+
+            cbdSeed.fill(0, 0, cbdSeed.size) //Security Feature
+
+            val systemVector = KyberMath.vectorAddition(
+                KyberMath.nttMatrixToVectorDot(matrix, secretVector, true),
+                noiseVector
+            )
+
+            val encryptionKeyBytes = ByteArray(parameter.ENCRYPTION_KEY_LENGTH - KyberConstants.N_BYTES) //Excluded nttSeed
+            val decryptionKeyBytes = ByteArray(parameter.DECRYPTION_KEY_LENGTH)
+
+            for(i in 0..<parameter.K) {
+                //Security Features
+                for(j in 0..<parameter.K) matrix[i][j].fill(0, 0, matrix[i][j].size)
+                noiseVector[i].fill(0, 0, noiseVector[i].size)
+
+                KyberMath.byteEncode(KyberMath.montVectorToVector(systemVector[i]), 12)
+                    .copyInto(encryptionKeyBytes, i * KyberConstants.ENCODE_SIZE)
+                KyberMath.byteEncode(KyberMath.montVectorToVector(secretVector[i]), 12)
+                    .copyInto(decryptionKeyBytes, i * KyberConstants.ENCODE_SIZE)
+            }
+
+            return KyberPKEKeyPair(
+                KyberEncryptionKey(parameter, encryptionKeyBytes, nttSeed),
+                KyberDecryptionKey(parameter, decryptionKeyBytes)
+            )
         }
     }
 }

src/commonMain/kotlin/asia/hombre/kyber/KyberConstants.kt
@@ -31,129 +31,127 @@ import kotlin.jvm.JvmField
  */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
-class KyberConstants private constructor() {
-    companion object {
-        /**
-         * Number of coefficients in the polynomial.
-         */
-        const val N: Int = 256
+object KyberConstants {
+    /**
+     * Number of coefficients in the polynomial.
+     */
+    const val N: Int = 256
 
-        /**
-         * The byte size of the number of coefficients in bits.
-         */
-        const val N_BYTES: Int = N shr 3
+    /**
+     * The byte size of the number of coefficients in bits.
+     */
+    const val N_BYTES: Int = N shr 3
 
-        /**
-         * Prime Integer composed of (2^8 * 13) + 1.
-         */
-        const val Q: Int = 3329
+    /**
+     * Prime Integer composed of (2^8 * 13) + 1.
+     */
+    const val Q: Int = 3329
 
-        /**
-         * Negated modular inverse of Q base 2^16
-         */
-        const val Q_INV: Int = -62209 //Generated using KyberMath.powMod(Q, -1, 2^16) and negated
+    /**
+     * Negated modular inverse of Q base 2^16
+     */
+    const val Q_INV: Int = -62209 //Generated using KyberMath.powMod(Q, -1, 2^16) and negated
 
-        /**
-         * Half of Q rounded to the closest whole integer.
-         */
-        const val Q_HALF: Int = 1665 //round(Q/2)
+    /**
+     * Half of Q rounded to the closest whole integer.
+     */
+    const val Q_HALF: Int = 1665 //round(Q/2)
 
-        /**
-         * Approximation of Q for Barrett Reduction
-         */
-        const val BARRETT_APPROX: Short = 20159 //(((1L shl 26) + (Q / 2)) / Q).toShort()
-        //const val MONT_R: Int = 1 shl 16
-        /**
-         * 2^16 * 2^16 (mod Q) for Montgomery Reduction.
-         */
-        const val MONT_R2: Short = 1353 //MONT_R.toLong() shl 16 <- mod Q = 1353 //Basically R^2 mod Q
+    /**
+     * Approximation of Q for Barrett Reduction
+     */
+    const val BARRETT_APPROX: Short = 20159 //(((1L shl 26) + (Q / 2)) / Q).toShort()
+    //const val MONT_R: Int = 1 shl 16
+    /**
+     * 2^16 * 2^16 (mod Q) for Montgomery Reduction.
+     */
+    const val MONT_R2: Short = 1353 //MONT_R.toLong() shl 16 <- mod Q = 1353 //Basically R^2 mod Q
 
-        /**
-         * The length of the Secret Key in bytes.
-         */
-        const val SECRET_KEY_LENGTH: Int = N_BYTES
+    /**
+     * The length of the Secret Key in bytes.
+     */
+    const val SECRET_KEY_LENGTH: Int = N_BYTES
 
-        /**
-         * Encoding size for encoding coefficients and terms.
-         */
-        const val ENCODE_SIZE: Int = 3 * N shr 1 //Sums up to 384
+    /**
+     * Encoding size for encoding coefficients and terms.
+     */
+    const val ENCODE_SIZE: Int = 3 * N shr 1 //Sums up to 384
 
-        /**
-         * Pre-generated Zeta values according to the formula 17^bitReverse(n) mod Q and converted to Montgomery Form.
-         */
-        @JvmField
-        val PRECOMPUTED_ZETAS_TABLE = intArrayOf(
-            1, 2571, 2970, 1812, 1493, 1422, 287, 202,
-            3158, 622, 1577, 182, 962, 2127, 1855, 1468,
-            573, 2004, 264, 383, 2500, 1458, 1727, 3199,
-            2648, 1017, 732, 608, 1787, 411, 3124, 1758,
-            1223, 652, 2777, 1015, 2036, 1491, 3047, 1785,
-            516, 3321, 3009, 2663, 1711, 2167, 126, 1469,
-            2476, 3239, 3058, 830, 107, 1908, 3082, 2378,
-            2931, 961, 1821, 2604, 448, 2264, 677, 2054,
-            2226, 430, 555, 843, 2078, 871, 1550, 105,
-            422, 587, 177, 3094, 3038, 2869, 1574, 1653,
-            3083, 778, 1159, 3182, 2552, 1483, 2727, 1119,
-            1739, 644, 2457, 349, 418, 329, 3173, 3254,
-            817, 1097, 603, 610, 1322, 2044, 1864, 384,
-            2114, 3193, 1218, 1994, 2455, 220, 2142, 1670,
-            2144, 1799, 2051, 794, 1819, 2475, 2459, 478,
-            3221, 3021, 996, 991, 958, 1869, 1522, 1628
-            /*1, 1729, 2580, 3289, 2642, 630, 1897, 848,
-            1062, 1919, 193, 797, 2786, 3260, 569, 1746,
-            296, 2447, 1339, 1476, 3046, 56, 2240, 1333,
-            1426, 2094, 535, 2882, 2393, 2879, 1974, 821,
-            289, 331, 3253, 1756, 1197, 2304, 2277, 2055,
-            650, 1977, 2513, 632, 2865, 33, 1320, 1915,
-            2319, 1435, 807, 452, 1438, 2868, 1534, 2402,
-            2647, 2617, 1481, 648, 2474, 3110, 1227, 910,
-            17, 2761, 583, 2649, 1637, 723, 2288, 1100,
-            1409, 2662, 3281, 233, 756, 2156, 3015, 3050,
-            1703, 1651, 2789, 1789, 1847, 952, 1461, 2687,
-            939, 2308, 2437, 2388, 733, 2337, 268, 641,
-            1584, 2298, 2037, 3220, 375, 2549, 2090, 1645,
-            1063, 319, 2773, 757, 2099, 561, 2466, 2594,
-            2804, 1092, 403, 1026, 1143, 2150, 2775, 886,
-            1722, 1212, 1874, 1029, 2110, 2935, 885, 2154*/
-        ) //Generated using Generators.generateZetas() test method
+    /**
+     * Pre-generated Zeta values according to the formula 17^bitReverse(n) mod Q and converted to Montgomery Form.
+     */
+    @JvmField
+    val PRECOMPUTED_ZETAS_TABLE = intArrayOf(
+        1, 2571, 2970, 1812, 1493, 1422, 287, 202,
+        3158, 622, 1577, 182, 962, 2127, 1855, 1468,
+        573, 2004, 264, 383, 2500, 1458, 1727, 3199,
+        2648, 1017, 732, 608, 1787, 411, 3124, 1758,
+        1223, 652, 2777, 1015, 2036, 1491, 3047, 1785,
+        516, 3321, 3009, 2663, 1711, 2167, 126, 1469,
+        2476, 3239, 3058, 830, 107, 1908, 3082, 2378,
+        2931, 961, 1821, 2604, 448, 2264, 677, 2054,
+        2226, 430, 555, 843, 2078, 871, 1550, 105,
+        422, 587, 177, 3094, 3038, 2869, 1574, 1653,
+        3083, 778, 1159, 3182, 2552, 1483, 2727, 1119,
+        1739, 644, 2457, 349, 418, 329, 3173, 3254,
+        817, 1097, 603, 610, 1322, 2044, 1864, 384,
+        2114, 3193, 1218, 1994, 2455, 220, 2142, 1670,
+        2144, 1799, 2051, 794, 1819, 2475, 2459, 478,
+        3221, 3021, 996, 991, 958, 1869, 1522, 1628
+        /*1, 1729, 2580, 3289, 2642, 630, 1897, 848,
+        1062, 1919, 193, 797, 2786, 3260, 569, 1746,
+        296, 2447, 1339, 1476, 3046, 56, 2240, 1333,
+        1426, 2094, 535, 2882, 2393, 2879, 1974, 821,
+        289, 331, 3253, 1756, 1197, 2304, 2277, 2055,
+        650, 1977, 2513, 632, 2865, 33, 1320, 1915,
+        2319, 1435, 807, 452, 1438, 2868, 1534, 2402,
+        2647, 2617, 1481, 648, 2474, 3110, 1227, 910,
+        17, 2761, 583, 2649, 1637, 723, 2288, 1100,
+        1409, 2662, 3281, 233, 756, 2156, 3015, 3050,
+        1703, 1651, 2789, 1789, 1847, 952, 1461, 2687,
+        939, 2308, 2437, 2388, 733, 2337, 268, 641,
+        1584, 2298, 2037, 3220, 375, 2549, 2090, 1645,
+        1063, 319, 2773, 757, 2099, 561, 2466, 2594,
+        2804, 1092, 403, 1026, 1143, 2150, 2775, 886,
+        1722, 1212, 1874, 1029, 2110, 2935, 885, 2154*/
+    ) //Generated using Generators.generateZetas() test method
 
-        /**
-         * Pre-generated Gamma values according to the formula 17^(2 * bitReverse(n)) mod Q and converted to Montgomery Form.
-         */
-        @JvmField
-        val PRECOMPUTED_GAMMAS_TABLE = intArrayOf(
-            2226, 1103, 430, 2899, 555, 2774, 843, 2486,
-            2078, 1251, 871, 2458, 1550, 1779, 105, 3224,
-            422, 2907, 587, 2742, 177, 3152, 3094, 235,
-            3038, 291, 2869, 460, 1574, 1755, 1653, 1676,
-            3083, 246, 778, 2551, 1159, 2170, 3182, 147,
-            2552, 777, 1483, 1846, 2727, 602, 1119, 2210,
-            1739, 1590, 644, 2685, 2457, 872, 349, 2980,
-            418, 2911, 329, 3000, 3173, 156, 3254, 75,
-            817, 2512, 1097, 2232, 603, 2726, 610, 2719,
-            1322, 2007, 2044, 1285, 1864, 1465, 384, 2945,
-            2114, 1215, 3193, 136, 1218, 2111, 1994, 1335,
-            2455, 874, 220, 3109, 2142, 1187, 1670, 1659,
-            2144, 1185, 1799, 1530, 2051, 1278, 794, 2535,
-            1819, 1510, 2475, 854, 2459, 870, 478, 2851,
-            3221, 108, 3021, 308, 996, 2333, 991, 2338,
-            958, 2371, 1869, 1460, 1522, 1807, 1628, 1701
-            /*17, 3312, 2761, 568, 583, 2746, 2649, 680,
-            1637, 1692, 723, 2606, 2288, 1041, 1100, 2229,
-            1409, 1920, 2662, 667, 3281, 48, 233, 3096,
-            756, 2573, 2156, 1173, 3015, 314, 3050, 279,
-            1703, 1626, 1651, 1678, 2789, 540, 1789, 1540,
-            1847, 1482, 952, 2377, 1461, 1868, 2687, 642,
-            939, 2390, 2308, 1021, 2437, 892, 2388, 941,
-            733, 2596, 2337, 992, 268, 3061, 641, 2688,
-            1584, 1745, 2298, 1031, 2037, 1292, 3220, 109,
-            375, 2954, 2549, 780, 2090, 1239, 1645, 1684,
-            1063, 2266, 319, 3010, 2773, 556, 757, 2572,
-            2099, 1230, 561, 2768, 2466, 863, 2594, 735,
-            2804, 525, 1092, 2237, 403, 2926, 1026, 2303,
-            1143, 2186, 2150, 1179, 2775, 554, 886, 2443,
-            1722, 1607, 1212, 2117, 1874, 1455, 1029, 2300,
-            2110, 1219, 2935, 394, 885, 2444, 2154, 1175*/
-        ) //Generated using Generators.generateGammas() test method
-    }
+    /**
+     * Pre-generated Gamma values according to the formula 17^(2 * bitReverse(n)) mod Q and converted to Montgomery Form.
+     */
+    @JvmField
+    val PRECOMPUTED_GAMMAS_TABLE = intArrayOf(
+        2226, 1103, 430, 2899, 555, 2774, 843, 2486,
+        2078, 1251, 871, 2458, 1550, 1779, 105, 3224,
+        422, 2907, 587, 2742, 177, 3152, 3094, 235,
+        3038, 291, 2869, 460, 1574, 1755, 1653, 1676,
+        3083, 246, 778, 2551, 1159, 2170, 3182, 147,
+        2552, 777, 1483, 1846, 2727, 602, 1119, 2210,
+        1739, 1590, 644, 2685, 2457, 872, 349, 2980,
+        418, 2911, 329, 3000, 3173, 156, 3254, 75,
+        817, 2512, 1097, 2232, 603, 2726, 610, 2719,
+        1322, 2007, 2044, 1285, 1864, 1465, 384, 2945,
+        2114, 1215, 3193, 136, 1218, 2111, 1994, 1335,
+        2455, 874, 220, 3109, 2142, 1187, 1670, 1659,
+        2144, 1185, 1799, 1530, 2051, 1278, 794, 2535,
+        1819, 1510, 2475, 854, 2459, 870, 478, 2851,
+        3221, 108, 3021, 308, 996, 2333, 991, 2338,
+        958, 2371, 1869, 1460, 1522, 1807, 1628, 1701
+        /*17, 3312, 2761, 568, 583, 2746, 2649, 680,
+        1637, 1692, 723, 2606, 2288, 1041, 1100, 2229,
+        1409, 1920, 2662, 667, 3281, 48, 233, 3096,
+        756, 2573, 2156, 1173, 3015, 314, 3050, 279,
+        1703, 1626, 1651, 1678, 2789, 540, 1789, 1540,
+        1847, 1482, 952, 2377, 1461, 1868, 2687, 642,
+        939, 2390, 2308, 1021, 2437, 892, 2388, 941,
+        733, 2596, 2337, 992, 268, 3061, 641, 2688,
+        1584, 1745, 2298, 1031, 2037, 1292, 3220, 109,
+        375, 2954, 2549, 780, 2090, 1239, 1645, 1684,
+        1063, 2266, 319, 3010, 2773, 556, 757, 2572,
+        2099, 1230, 561, 2768, 2466, 863, 2594, 735,
+        2804, 525, 1092, 2237, 403, 2926, 1026, 2303,
+        1143, 2186, 2150, 1179, 2775, 554, 886, 2443,
+        1722, 1607, 1212, 2117, 1874, 1455, 1029, 2300,
+        2110, 1219, 2935, 394, 885, 2444, 2154, 1175*/
+    ) //Generated using Generators.generateGammas() test method
 }

commit 02292e920320ff68242e91b098aa7e537a6dcd0f

Update the method name for the internal NTT and invNTT methods * This follows the format for Java/Kotlin method names where the first letter should be lowercase. * NTT() -> ntt() * invNTT() -> nttInv() * Fixes errors in Tests.kt introduced by 69ff060e.

src/commonMain/kotlin/asia/hombre/kyber/KyberAgreement.kt
@@ -84,7 +84,7 @@ class KyberAgreement(private val decapsulationKey: KyberDecapsulationKey) {
                     parameter.ETA1,
                     KyberMath.prf(parameter.ETA1, randomness, n.toByte())
                 )
-                randomnessVector[i] = KyberMath.NTT(randomnessVector[i])
+                randomnessVector[i] = KyberMath.ntt(randomnessVector[i])
 
                 noiseVector[i] = KyberMath.samplePolyCBD(
                     parameter.ETA2,
@@ -107,7 +107,7 @@ class KyberAgreement(private val decapsulationKey: KyberDecapsulationKey) {
 
             var constantTerm = IntArray(KyberConstants.N)
             for(i in 0..<parameter.K) {
-                coefficients[i] = KyberMath.invNTT(coefficients[i])
+                coefficients[i] = KyberMath.nttInv(coefficients[i])
                 coefficients[i] = KyberMath.vectorToVectorAdd(coefficients[i], noiseVector[i])
 
                 constantTerm = KyberMath.vectorToVectorAdd(constantTerm, KyberMath.multiplyNTTs(nttKeyVector[i], randomnessVector[i]))
@@ -119,7 +119,7 @@ class KyberAgreement(private val decapsulationKey: KyberDecapsulationKey) {
                 randomnessVector[i].fill(0, 0, randomnessVector[i].size)
             }
 
-            constantTerm = KyberMath.invNTT(constantTerm)
+            constantTerm = KyberMath.nttInv(constantTerm)
             constantTerm = KyberMath.vectorToVectorAdd(constantTerm, noiseTerm)
             constantTerm = KyberMath.vectorToVectorAdd(constantTerm, muse)
 
@@ -172,12 +172,12 @@ class KyberAgreement(private val decapsulationKey: KyberDecapsulationKey) {
             val secretVector = KyberMath.byteDecode(decryptionKey.keyBytes, 12)
 
             for (i in 0..<parameter.K) {
-                val subtraction = KyberMath.invNTT(
+                val subtraction = KyberMath.nttInv(
                     KyberMath.multiplyNTTs(
                         KyberMath.vectorToMontVector(secretVector.copyOfRange(
                             i * KyberConstants.N,
                             (i + 1) * KyberConstants.N)),
-                        KyberMath.NTT(coefficients[i])
+                        KyberMath.ntt(coefficients[i])
                     )
                 )
                 for (j in 0..<KyberConstants.N)

src/commonMain/kotlin/asia/hombre/kyber/KyberKeyGenerator.kt
@@ -118,13 +118,13 @@ object KyberKeyGenerator {
                     parameter.ETA1,
                     KyberMath.prf(parameter.ETA1, cbdSeed, nonce.toByte())
                 )
-                secretVector[i] = KyberMath.NTT(secretVector[i])
+                secretVector[i] = KyberMath.ntt(secretVector[i])
 
                 noiseVector[i] = KyberMath.samplePolyCBD(
                     parameter.ETA1,
                     KyberMath.prf(parameter.ETA1, cbdSeed, (nonce + parameter.K).toByte())
                 )
-                noiseVector[i] = KyberMath.NTT(noiseVector[i])
+                noiseVector[i] = KyberMath.ntt(noiseVector[i])
             }
 
             cbdSeed.fill(0, 0, cbdSeed.size) //Security Feature

src/commonMain/kotlin/asia/hombre/kyber/internal/KyberMath.kt
@@ -245,7 +245,7 @@ internal object KyberMath {
     }
 
     @JvmSynthetic
-    fun NTT(polynomials: IntArray): IntArray {
+    fun ntt(polynomials: IntArray): IntArray {
         val output = polynomials.copyOf()
 
         var k = 1
@@ -272,7 +272,7 @@ internal object KyberMath {
     }
 
     @JvmSynthetic
-    fun invNTT(nttPolynomials: IntArray): IntArray {
+    fun nttInv(nttPolynomials: IntArray): IntArray {
         val output = nttPolynomials.copyOf()
 
         var k = (KyberConstants.N shr 1) - 1

src/commonTest/kotlin/asia/hombre/kyber/Tests.kt
@@ -19,7 +19,7 @@
 package asia.hombre.kyber
 
 import asia.hombre.kyber.internal.KyberMath
-import asia.hombre.kyber.internal.KyberMath.Companion.int
+import asia.hombre.kyber.internal.KyberMath.int
 import org.kotlincrypto.SecureRandom
 import kotlin.math.abs
 import kotlin.random.Random
@@ -338,8 +338,8 @@ class Tests {
     @Test
     fun ntt() {
         val vectors = generateRandom256Shorts()
-        val nttVectors = KyberMath.NTT(vectors)
-        val recoveredVectors = KyberMath.invNTT(nttVectors)
+        val nttVectors = KyberMath.ntt(vectors)
+        val recoveredVectors = KyberMath.nttInv(nttVectors)
 
         assertContentEquals(vectors, recoveredVectors, "Conversion to NTT and inversion failed!")
     }
@@ -348,8 +348,8 @@ class Tests {
     fun ntt_comprehensive() {
         for(i in 0..KyberConstants.Q) {
             val vectors = IntArray(KyberConstants.N) { i % KyberConstants.Q }
-            val nttVectors = KyberMath.NTT(vectors)
-            val recoveredVectors = KyberMath.invNTT(nttVectors)
+            val nttVectors = KyberMath.ntt(vectors)
+            val recoveredVectors = KyberMath.nttInv(nttVectors)
 
             assertContentEquals(vectors, recoveredVectors, "Comprehensive NTT conversion and inversion failed!")
         }